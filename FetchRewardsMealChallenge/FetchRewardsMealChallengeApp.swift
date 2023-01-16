ce4b377a077930923e710eadb440973cee10fb5b
Angel <angelramirez@artexcomputer.com>
2023-01-16 16:51:41 -0500

    Initial Commit

---
+++ FetchRewardsMealChallenge/FetchRewardsMealChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRewardsMealChallengeApp.swift
+//  FetchRewardsMealChallenge
+//
+//  Created by ANGEL RAMIREZ on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchRewardsMealChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
